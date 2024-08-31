ee852673f92e731c371405da6dc2caa71f754ae6
Hendry535230151 <hendry.535230151@stu.untar.ac.id>
2024-09-01 00:11:23 +0700

    sixth commit

---
+++ js/CV.js
@@ -0,0 +1,6 @@
+function openContent(){
+    const container_main = document.getElementById("sub-container");
+    const container_2 = document.getElementById("sub-container-2");
+    container_main.classList.toggle("hide-content-1");
+    container_2.classList.toggle("hide-content-2");
+}
